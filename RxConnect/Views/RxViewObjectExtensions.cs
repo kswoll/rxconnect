00df1dba6c8cd060008a3bf435daa4f9e5cd452f
Kirk Woll <kirk.woll@gmail.com>
2015-07-15 14:29:52 -0700

    Try to implement the setter on the Connect method

--- RxConnect/Views/RxViewObjectExtensions.cs
+++ RxConnect/Views/RxViewObjectExtensions.cs
@@ -28,9 +28,36 @@ namespace RxConnect.Views
                 propertyPath[i + 1] = (PropertyInfo)remainingPath[i];
             }
 
+            var setMainMember = viewProperty.Body as MemberExpression;
+            if (setMainMember == null)
+                throw new ArgumentException("Lambda expression must specify a property path of the form (Foo.Bar.FooBar)", "viewProperty");
+
             Func<Action<TViewValue>> createSetValue = () =>
             {
-                return null;
+                var currentMember = setMainMember;
+                var currentTarget = currentMember.Expression;
+                Expression body = null;
+                do
+                {
+                    var currentTargetMemberAccess = currentTarget as MemberExpression;
+                    if (currentTargetMemberAccess == null)
+                    {
+                        var rootTarget = currentTarget as ParameterExpression;
+                        if (rootTarget == null)
+                            throw new ArgumentException("Root of lambda expression (" + viewProperty + ") must be a reference to the parameter passed into the lambda.");
+
+                        body = Expression.MakeMemberAccess(Expression.Constant(view), currentMember.Member);
+                        break;
+                    }
+                    else 
+                    {
+                        currentMember = currentTargetMemberAccess;
+                    }
+                }
+                while (true);
+
+                var lambda = Expression.Lambda<Action<TViewValue>>(body);
+                return lambda.Compile();
             };
             Lazy<Action<TViewValue>> setValue = new Lazy<Action<TViewValue>>(createSetValue);
 
