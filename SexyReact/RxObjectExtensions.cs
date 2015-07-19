fab93295737d6f83f97a7657443de8361c947e67
Kirk Woll <kirk.woll@gmail.com>
2015-07-19 10:15:16 -0700

    Add some documentation

--- SexyReact/RxObjectExtensions.cs
+++ SexyReact/RxObjectExtensions.cs
@@ -12,6 +12,17 @@ namespace SexyReact
         private static readonly MethodInfo observePropertyMethod = typeof(IRxObject).GetMethod("ObserveProperty");
         private static readonly MethodInfo combineMethod = typeof(RxObjectExtensions).GetMethod("Combine", BindingFlags.Static | BindingFlags.NonPublic);
 
+        /// <summary>
+        /// Produces an observable that returns the current value of the specified property as its value changes.  This handles
+        /// property paths, so if you specify `x.Foo.Bar` and `x.Foo` is initially null, then the initial value of the observable
+        /// is defualt(TValue).  When `Foo` becomes non-null the observable will emit the current value of `Bar`.  Finally, if 
+        /// `Foo` becomes null, the observable again emits default(TValue).
+        /// </summary>
+        /// <returns>The property path that identifies the value of the property for the observable.</returns>
+        /// <param name="obj">The IRxObject that contains the initial property in the path.</param>
+        /// <param name="propertyPath">The sequence of properties that leads to the value.</param>
+        /// <typeparam name="T">The type that contains the initial property in the path.</typeparam>
+        /// <typeparam name="TValue">The value of the terminal property in the path.</typeparam>
         public static IObservable<TValue> ObserveProperty<T, TValue>(this T obj, params MemberInfo[] propertyPath)
             where T : IRxObject
         {
@@ -39,6 +50,17 @@ namespace SexyReact
             return lastObservable;
         }
 
+        /// <summary>
+        /// Produces an observable that returns the current value of the specified property as its value changes.  This handles
+        /// property paths, so if you specify `x.Foo.Bar` and `x.Foo` is initially null, then the initial value of the observable
+        /// is defualt(TValue).  When `Foo` becomes non-null the observable will emit the current value of `Bar`.  Finally, if 
+        /// `Foo` becomes null, the observable again emits default(TValue).
+        /// </summary>
+        /// <returns>The property path that identifies the value of the property for the observable.</returns>
+        /// <param name="obj">The IRxObject that contains the initial property in the path.</param>
+        /// <param name="propertyPath">The sequence of properties that leads to the value.</param>
+        /// <typeparam name="T">The type that contains the initial property in the path.</typeparam>
+        /// <typeparam name="TValue">The value of the terminal property in the path.</typeparam>
         public static IObservable<TValue> ObserveProperty<T, TValue>(this T obj, Expression<Func<T, TValue>> property)
             where T : IRxObject
         {
@@ -66,13 +88,28 @@ namespace SexyReact
                 .Switch();
         }
 
+        /// <summary>
+        /// Updates the value of the specified property based on the values that come down the observable.
+        /// </summary>
+        /// <param name="obj">The object whose property should be updated.</param>
+        /// <param name="observable">The observable whose sequence of values will be used to update the value of the property.</param>
+        /// <param name="property">The property whose value should be updated by the observabe.</param>
+        /// <typeparam name="T">The type of the object that contains the property.</typeparam>
+        /// <typeparam name="TValue">The property type of the properyt.</typeparam>
         public static void ObservableAsProperty<TValue>(this IRxObject obj, IObservable<TValue> observable, PropertyInfo property)
         {
             obj.Register(observable.DistinctUntilChanged().Subscribe(x => obj.Set(property, x)));
         }
 
+        /// <summary>
+        /// Updates the value of the specified property based on the values that come down the observable.
+        /// </summary>
+        /// <param name="obj">The object whose property should be updated.</param>
+        /// <param name="observable">The observable whose sequence of values will be used to update the value of the property.</param>
+        /// <param name="property">The property whose value should be updated by the observabe.</param>
+        /// <typeparam name="T">The type of the object that contains the property.</typeparam>
+        /// <typeparam name="TValue">The property type of the properyt.</typeparam>
         public static void ObservableAsProperty<T, TValue>(this T obj, IObservable<TValue> observable, Expression<Func<T, TValue>> property) 
-            where T : IRxObject
         {
             var memberExpression = property.Body as MemberExpression;
             if (memberExpression == null)
