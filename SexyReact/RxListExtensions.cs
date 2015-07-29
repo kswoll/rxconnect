8c8f7ce0d6c27b882d038c87113127512e7a431c
Kirk Woll <kirk.woll@gmail.com>
2015-07-29 11:32:02 -0700

    Add some docs to the list extensions

--- SexyReact/RxListExtensions.cs
+++ SexyReact/RxListExtensions.cs
@@ -6,12 +6,34 @@ namespace SexyReact
 {
     public static class RxListExtensions
     {
+        /// <summary>
+        /// Returns an observable that fires whenever a value for an element currently in the list changes.  This means that if
+        /// an element was in the list when this method is called but subsequently removed, further changes to the property on 
+        /// that element will not propagate down this observable.
+        /// </summary>
+        /// <typeparam name="T">The type of the elements of the list</typeparam>
+        /// <typeparam name="TValue">The type of the value returned by the selector</typeparam>
+        /// <param name="list">The list on which to observe elements</param>
+        /// <param name="selector">Identifies a property path to a value on the current element</param>
+        /// <returns>The observable that monitors changes to a property for elements currently in the list</returns>
         public static IObservable<RxListObservedElement<T, TValue>> ObserveElement<T, TValue>(this IRxList<T> list, Expression<Func<T, TValue>> selector)
             where T : IRxObject
         {
             return new RxListElementObservable<T, TValue>(list, selector);
         }
 
+        /// <summary>
+        /// Creates a list whose contents reflect the contents of the source list, but transformed in some way by the selector.  This is a 
+        /// live list and will change accordingly when the source list is modified.
+        /// </summary>
+        /// <typeparam name="TSource">The type of the elements in the derived list</typeparam>
+        /// <typeparam name="T">The type of the elements in the source list</typeparam>
+        /// <param name="source">The source list from which the derived list's contents will be derived</param>
+        /// <param name="selector">The contents of the derived list are determined by applying this selector to each element in 
+        /// the source list</param>
+        /// <param name="removed">If not null, fired when an element from the source list is either removed or modified.  You 
+        /// can take cleanup actions for the old items by using this callback.</param>
+        /// <returns></returns>
         public static RxDerivedList<T> Derive<TSource, T>(this IRxList<TSource> source, Func<TSource, T> selector, Action<T> removed = null)
         {
             var locker = new object();
