13250420c6ca7688b891bd7107c82dead726938d
Kirk Woll <kirk.woll@gmail.com>
2015-08-16 10:13:14 -0700

    Upgrade fody plugin for sample

--- SexyReact/RxAttribute.cs
+++ SexyReact/RxAttribute.cs
@@ -6,10 +6,9 @@ namespace SexyReact
     /// Decorate on your properties in your rx objects.  You implement them with auto-properties, but
     /// under the hood they get implemented by efficient calls to Get/Set (no reflection).
     /// 
-    /// Future plans: Decorate on your classes to implement more efficient storage/property-observer
-    /// strategies.
+    /// Decorate on your classes to implement more efficient storage/property-observer strategies.
     /// </summary>
-    [AttributeUsage(AttributeTargets.Property)]
+    [AttributeUsage(AttributeTargets.Property | AttributeTargets.Class)]
     public class RxAttribute : Attribute
     {
     }
