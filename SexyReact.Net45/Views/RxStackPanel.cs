4015a5b074f951c197ba2044882e2ab9dec9bbf3
Kirk Woll <kirk.woll@gmail.com>
2016-01-05 17:53:03 -0800

    Add rxified StackPanel

---
+++ SexyReact.Net45/Views/RxStackPanel.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Reflection;
+using System.Windows.Controls;
+
+namespace SexyReact.Views
+{
+    public class RxStackPanel<T> : StackPanel, IRxViewObject<T> 
+        where T : IRxObject
+    {
+        private IRxViewObject<T> mixin = new RxViewObject<T>();
+        private bool isDisposed;
+
+        public void Register(IDisposable disposable) => mixin.Register(disposable);
+        public TValue Get<TValue>(PropertyInfo property) => mixin.Get<TValue>(property);
+        public void Set<TValue>(PropertyInfo property, TValue value) => mixin.Set(property, value);
+        public IObservable<TValue> ObserveProperty<TValue>(PropertyInfo property) => mixin.ObserveProperty<TValue>(property);
+        public IObservable<IPropertyChanging> Changing => mixin.Changing;
+        public IObservable<IPropertyChanged> Changed => mixin.Changed;
+
+        public RxStackPanel()
+        {
+            Unloaded += (sender, args) => Dispose();
+        }
+
+        object IRxViewObject.Model
+        {
+            get { return Model; }
+            set { Model = (T)value; }
+        }
+
+        public T Model
+        {
+            get { return mixin.Model; }
+            set
+            {
+                mixin.Model = value;
+                DataContext = value;
+            }
+        }
+
+        public void Dispose()
+        {
+            if (!isDisposed)
+            {
+                isDisposed = true;
+                Dispose(true);
+            }
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                mixin.Dispose();
+            }
+        }
+    }
+}
